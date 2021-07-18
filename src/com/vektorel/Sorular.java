c0b583cce11f75d3d813b62db35c4b124106d13a
Vektorel <Vektorel@LAB8-OGRETMEN>
2021-07-18 18:06:19 +0300

    sorular eklendi

--- src/com/vektorel/Sorular.java
+++ src/com/vektorel/Sorular.java
@@ -1,8 +1,34 @@
 package com.vektorel;
 
+import java.util.Random;
+
 public class Sorular {
 
 	public static void main(String[] args) {
+		// s1-Kullanýcýnýn girmiþ olduðu bir sayý listesinde
+		// enbüyük ve en küçük sayýlarý bulan program.
+		// s2-Bir dizinin içindeki sayýlarý sýralamak
+		// s3- aþaðýda gördüðünüz þekli for kullanarak yazýnýz.
+		//  *  *  *
+		//  *  *  *
+		//  *  *  *
+		// s4- yukarýda ki þekli kullanýcýdan deðer alarak 
+		// kaç sayý var ise ona göre büyüyen bir kare çizen program.
+		// 3X3 , 5X5, 20X20
+		// s5- kullanýcýdan girdi ile Ýçi boþ olan kare oluþturun 
+		// * * *
+		// *   *
+		// * * *
+		// s6- Aþaðýda örnek verilen yapý ile bir oyun yapýn.
+		// kullanýcýnýn 3 hakký olacak sayý tahmin edecek.
+		// son hakký bittiðinde kaç yaklaþýk bulduðunu yazýnýz.
+		// not: sayý 60 olsun: k. 32, yanlýþ, yukarý		
+		Random rd = new Random();
+		System.out.println("rastgele sayý....: "+rd.nextInt(100));
+		// 
+		//
+		//
+		//
 		
 
 	}
